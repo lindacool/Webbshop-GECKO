54e45e919efeccf8adc44cf5d0d1272b2ba1bad9
Marcus Enblom <marcusenblom@gmail.com>
2020-03-02 14:23:18 +0100

    Admin can now edit products

--- router/adminPageRouting.js
+++ router/adminPageRouting.js
@@ -33,11 +33,24 @@ router.post('/admin', async (req, res)=>{
     res.redirect('admin')
 });
 
-router.get("/products/:id", async (req, res) => {
+// Edit product
+router.post("/edit/:id", async (req, res) => {
 
-    const clickedProduct = await Product.findById( {_id: req.params.id} );
+    await Product.updateOne( {_id: req.params.id}, {
+        $set: {
+            title: req.body.title,
+            imgUrl: req.body.imgUrl,
+            price: req.body.price,
+            description: req.body.description,
+            size: req.body.size,
+            newArrival: req.body.newArrival = Boolean(req.body.newArrival),
+            topSeller: req.body.topSeller = Boolean(req.body.topSeller),
+            male: req.body.male = Boolean(req.body.male),
+            female: req.body.female = Boolean(req.body.female)
+        }
+    });
     
-    res.render("detailProductPage", { clickedProduct });
+    res.redirect("/admin");
     
 });
 
