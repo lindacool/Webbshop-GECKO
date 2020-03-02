2c0222924eea5c09be8f9978975eade17f49b8c3
Linda Andersson <lindaandersson@Lindas-MBP.mediegruppen.se>
2020-03-02 12:05:20 +0100

    scss

--- router/startPageRouting.js
+++ router/startPageRouting.js
@@ -4,7 +4,6 @@ const Product = require('../model/productModel');
 const router = express();
 
 router.get('/', async (req, res) => {
-    console.log("hejdjfhejejeij xxxxxxxxxxxxxxxxxxxxxx")
     const newArrivals = await Product.find();
     res.render('startpage', {
         newArrivals
