8dd89646adbebf3b19f7acda42cd0bfdafd5a7fc
Love <ottenby123@gmail.com>
2020-03-03 14:34:55 +0100

    nav

--- public/nav.js
+++ public/nav.js
@@ -1,6 +1,12 @@
 $(document).ready(function () {
     
     $('.hambutton').on('click', function () {
-        $("nav").slideToggle();
+        if($(".nav").hasClass('navShow')) {
+            $(".nav").removeClass('navShow');
+        } else {
+            $(".nav").addClass('navShow');
+            
+        }
+        
     })
 });
