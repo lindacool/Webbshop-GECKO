c0a031e1185dbbf2f3ebcd61e8f43d81df07cb4d
Love Ottenby <ottenby123@gmail.com>
2020-03-16 10:53:18 +0100

    prutt

--- router/verifyAdminToken.js
+++ router/verifyAdminToken.js
@@ -1,27 +1,5 @@
 const jwt = require("jsonwebtoken");
 
-<<<<<<< HEAD
-module.exports = (req, res, next)=>{
-
-  const token = req.cookies.jsonwebtoken;
-
-  if(token) {
-    //decode token och user info
-    const admin = jwt.verify(token, "secretkey");
-    console.log("user info" , admin);
-    req.admin = admin;
-    next();
-  }
-  else {
-    res.send("You are not authorised");
-  }
-}
-//req.cookies.jsonwebtoken
-  // kollar om användare har cookies 
-  // jwt verifiering metod  för att kolla om det är en valid cookies
-  // validerings data till server 
-  // next()
-=======
 module.exports = (req, res, next) => {
 
   const token = req.cookies.jsonwebtoken;
@@ -36,4 +14,3 @@ module.exports = (req, res, next) => {
     res.send("You are not authorised");
   }
 }
->>>>>>> e0c86e0dcc829f2e71cd414b7f38827af3e28393
